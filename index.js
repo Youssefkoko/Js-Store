80dbd6d403367d1d10d6c485bdf2979af14d99b5
Youssef Khoukhou <youssefkoko01@gmail.com>
2020-11-26 14:59:07 +0100

    Creating Local storage for products page

---
+++ index.js
@@ -0,0 +1,26 @@
+// global imports
+import './src/toggleSidebar.js';
+import './src/cart/toggleCart.js';
+import './src/cart/setupCart.js';
+// specific imports
+import fetchProducts from './src/fetchProducts.js';
+// Store just an array of products returned from that file 
+import { setupStore, store } from './src/store.js';
+import display from './src/displayProducts.js';
+import { getElement } from './src/utils.js';
+
+const init = async () => {
+  const products = await fetchProducts();
+  if(products){
+    // Add products to store 
+    setupStore(products)
+    const featured = store.filter((product) => {
+      if(product.featured === true){
+        return product;
+      }
+    });
+    console.log(featured);
+  }
+  
+};
+window.addEventListener('DOMContentLoaded', init);
