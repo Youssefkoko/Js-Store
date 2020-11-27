4f235286dd1787c5b4d8b2f6681a7ea5afd326c6
Youssef Khoukhou <youssefkoko01@gmail.com>
2020-11-27 14:44:06 +0100

    create functions for filter: price,search,companies.

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -3,7 +3,28 @@ import display from '../displayProducts.js';
 
 const setupCompanies = (products) => {
   let companies = ['All', ...new Set(products.map(product => product.company))]
-  console.log(companies);
+  
+  const companiesDOM = getElement('.companies');
+  companiesDOM.innerHTML = companies.map((company) => {
+    
+  return `<button class="company-btn">${company}</button>`
+  }).join('');
+  companiesDOM.addEventListener('click', function(e){
+    const target = e.target;
+    if(target.classList.contains('company-btn')){
+      let newStore = [];
+      if(target.textContent === 'all'){
+        newStore = [...Products];
+      }else{
+        newStore = products.filter((product) => {
+        
+          return product.company === target.textContent 
+        });
+      }
+      display(newStore, getElement('.products-container'));
+    }
+
+  })
 };
 
 export default setupCompanies;
--- src/filters/price.js
+++ src/filters/price.js
@@ -1,6 +1,31 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
-const setupPrice = () => {};
+const setupPrice = (store) => {
+  const priceInput = getElement('.price-filter');
+  const priceValue = getElement('.price-value')
+//  sETUP FILTER 
+  let maxPrice = store.map((product)=> {
+    return product.price;
+  })
+  maxPrice = Math.max(...maxPrice);
+  maxPrice = Math.ceil(maxPrice / 100);
+  priceInput.value = maxPrice;
+  priceInput.max = maxPrice;
+  priceInput.min = 0;
+  console.log( maxPrice );
+  priceInput.addEventListener('input', function(){
+    let value = parseInt( priceInput.value);
+    priceValue.textContent = `The value: €${value}`;
+    let newStore = store.filter((product)=> {
+      return product.price / 100 <= value;
+    })
+    display(newStore, getElement('.products-container'));
+    if(newStore.length < 1){
+      getElement('.products-container').innerHTML = `<h3 class="filter-error">No Product Match your price.</h3>`
+    }
+    
+  })
+};
 
 export default setupPrice;
