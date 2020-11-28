daa5d45def4208258013701e3dcd46ccbc67f6e5
Youssef Khoukhou <youssefkoko01@gmail.com>
2020-11-28 02:13:45 +0100

    check product on the LocalStorage and update it

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -8,10 +8,29 @@ import {
 import { openCart } from './toggleCart.js';
 import { findProduct } from '../store.js';
 import addToCartDOM from './addToCartDOM.js';
+const cartItemCountDOM = getElement('.cart-item-count');
+const cartItemsDOM = getElement('.cart-items');
+const cartTotalDOM = getElement('.cart-total'); 
 // set items
+let cart = getStorageItem('cart');
 
 export const addToCart = (id) => {
-  console.log(id);
+  const item = cart.find(item => item.id === id);
+  if(!item){
+    let product = findProduct(id);
+    product = {...product, amount:0};
+    cart = [...cart, product];
+    // Add to the DOM 
+    addToCartDOM(product);
+        
+  }else{
+
+  }
   openCart();
 
 };
+
+const init = () =>{
+  
+}
+init();
